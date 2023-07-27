d99daf583960081cb66858ccf301d31efeaa7d63
Jesús Gutierrez <jgutierrezyancan@outlook.com>
2023-07-26 20:28:27 -0500

    change header
    header changed to show a basic one

---
+++ src/components/MainFooter/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MainFooter = () => {
+  // static propTypes = {}
+
+  return (
+    <div>
+      <h1>Footer</h1>
+    </div>
+  );
+};
+
+export default MainFooter;
