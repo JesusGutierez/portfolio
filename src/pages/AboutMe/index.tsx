08c991b0e067cd12c55dc9a363a226dee20c7c7f
Jesús Gutierrez <jgutierrezyancan@outlook.com>
2023-09-13 16:51:47 -0500

    perfil redactado

--- src/pages/AboutMe/index.tsx
+++ src/pages/AboutMe/index.tsx
@@ -1,20 +1,23 @@
+import calculateAge from '@/utils/general';
 import React from 'react';
 
 function AboutMe() {
+  const age: number = calculateAge(new Date('2001-11-23'));
   return (
     <div
       className="heightWithHeader flex items-center justify-center"
       id="about"
     >
       <div className="flex p-10">
-        <div className="min-w-[300px] h-[300px] bg-blue-900 "></div>
-        <div className="max-w-[450px] ml-[20px]">
+        <div className="w-[250px] h-[250px] bg-blue-900 "></div>
+        <div className="max-w-[500px] ml-[20px]">
           <p className="text-2xl">
             <span className="skills-table text-third text-3xl">HI!</span>
             <br />I am <span className="text-third">Jesús Gutierrez</span>, a{' '}
-            <span className="text-third">Front end developer</span>, my date of
-            birth is 23-11-2001, am peruvian and interested on AI and machine
-            learning.
+            <span className="text-third">Software developer</span> in Lima, Peru
+            with experience developing web and mobile interfaces using{' '}
+            <span className="text-third">Javascript frameworks</span>; and I am
+            interested in the AI and artificial intelligence branch.
           </p>
         </div>
       </div>
