72c1db310afcabf95d2e98152cfe3943fc4df2df
Jesús Gutierrez <jgutierrezyancan@outlook.com>
2023-09-13 17:18:15 -0500

    aboutme done

--- src/pages/AboutMe/index.tsx
+++ src/pages/AboutMe/index.tsx
@@ -1,5 +1,7 @@
 import calculateAge from '@/utils/general';
 import React from 'react';
+import Image from 'next/image';
+import profileImg from '/src/assets/profile.jpg';
 
 function AboutMe() {
   const age: number = calculateAge(new Date('2001-11-23'));
@@ -9,8 +11,14 @@ function AboutMe() {
       id="about"
     >
       <div className="flex p-10">
-        <div className="w-[250px] h-[250px] bg-blue-900 "></div>
-        <div className="max-w-[500px] ml-[20px]">
+        <div className="w-[250px] h-[250px]">
+          <Image
+            src={profileImg}
+            alt="jesús antonio gutierrez yancán photo"
+          ></Image>
+        </div>
+
+        <div className="flex-1 max-w-[550px] ml-[30px]">
           <p className="text-2xl">
             <span className="skills-table text-third text-3xl">HI!</span>
             <br />I am <span className="text-third">Jesús Gutierrez</span>, a{' '}
