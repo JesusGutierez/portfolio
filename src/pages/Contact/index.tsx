3aaa4b3caf98751f82801793cb59fa62e91d2035
Jesús Gutierrez <jgutierrezyancan@outlook.com>
2023-09-13 00:22:21 -0500

    formulario funcional

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,14 +1,16 @@
 import Title from '@/components/Title';
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './index.module.scss';
 import { AiOutlineGithub, AiFillLinkedin, AiOutlineMail } from 'react-icons/ai';
 import { SocialNetwork } from '@/models/SocialNetwork';
 import { Field } from '@/models/FIeld';
 import { sendEmail } from '@/services/email';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 const fields: Field[] = [
   new Field('name', null),
-  new Field('e-mail', null),
+  new Field('email', null),
   new Field('subject', null),
   new Field('message', 6),
 ];
@@ -29,24 +31,28 @@ const networks: SocialNetwork[] = [
     <AiOutlineMail />,
     'Email',
     'gtrrzyancan@outlook.com',
-    'gtrrzyancan@outlook.com'
+    'mailto:gtrrzyancan@outlook.com'
   ),
 ];
 
-const objFields = (formFields: Field[]) => {
-  const obj: Map<string, string> = new Map();
-  formFields.forEach((field, index) => obj.set(field.name, ''));
-  console.log('pbj', obj);
-  return obj;
+const objFields = (formFields: Field[]): Map<string, string> => {
+  const newObj: Map<string, string> = new Map<string, string>();
+  formFields.forEach((formField, index) => newObj.set(formField.name, ''));
+  return newObj;
 };
 
 function Contact() {
-  const [formData, setFormData] = useState(objFields(fields));
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    alert(formData);
-  };
+  const formik = useFormik({
+    initialValues: objFields(fields),
+    onSubmit: (values: any) => {
+      alert(JSON.stringify(values));
+      sendEmail(
+        ['gtrrzyancan@outlook.com'],
+        'Message from Portfolio',
+        values.message
+      );
+    },
+  });
 
   return (
     <div className="flex flex-col heightWithHeader" id="contact">
@@ -54,7 +60,7 @@ function Contact() {
       <div id={styles.infoContainer}>
         <div>
           <p className="mb-5 text-xl">Leave ne a messgage</p>
-          <form method="post" onSubmit={handleSubmit}>
+          <form onSubmit={formik.handleSubmit}>
             {fields.map((field, index) => {
               return (
                 <div
@@ -67,17 +73,21 @@ function Contact() {
                   >
                     <p className="capitalize">{field.name}</p>
                   </label>
+
                   <div className="bg-secondary p-1 rounded w-full">
                     {field.nroLines != null ? (
                       <textarea
                         className="appearance-none rounded py-2 px-3 bg-third w-full text-secondary"
+                        id={field.name}
                         rows={field.nroLines}
+                        {...formik.getFieldProps(field.name)}
                       ></textarea>
                     ) : (
                       <input
                         className="appearance-none rounded py-2 px-3 bg-third w-full text-secondary"
                         id={field.name}
                         type="text"
+                        {...formik.getFieldProps(field.name)}
                       />
                     )}
                   </div>
@@ -87,14 +97,7 @@ function Contact() {
             <div className="flex justify-end">
               <button
                 className="bg-third text-secondary hover:bg-blue-700 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="button"
-                onClick={() => {
-                  sendEmail(
-                    ['gtrrzyancan@outlook.com'],
-                    'New message - Portfolio - Jesús',
-                    'I want to invite you for an interview'
-                  );
-                }}
+                type="submit"
               >
                 Send!
               </button>
